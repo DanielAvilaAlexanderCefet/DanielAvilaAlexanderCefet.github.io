a678e7cc71da89ac169e64536c8ac7b3eecea4b8
AM <yannatsuny@gmail.com>
2025-06-13 21:56:39 -0300

    LabFunc

---
+++ LabFunc/script.js
@@ -0,0 +1,48 @@
+function saudacao() {
+
+console.log("Olá, bem-vindo ao sistema!");
+
+}
+function HoraAtual(){
+    hoje = new Date
+    console.log(hoje.getDate()+"/"+hoje.getMonth()+"/"+hoje.getYear())
+}
+function calcularAreaRetangulo(largura, altura){
+    var total = largura * altura
+    console.log(total)
+}
+
+function ehPar(num){
+    if(num%2 == 0)
+        console.log(true)
+    else
+        console.log(false)
+}
+function calcularMedia(n1, n2, n3) {
+
+    return (n1 + n2 + n3) / 3;
+    
+    }
+    
+function verificarSituacao(media) {
+    
+    if (media >= 7) return "Aprovado";
+    
+    if (media >= 5) return "Em recuperação";
+    
+    return "Reprovado";
+    
+}
+    
+    let media = calcularMedia(6.5, 8.0, 7.0);
+    
+    console.log(`Média: ${media.toFixed(2)} - Situação: ${verificarSituacao(media)}`);
+
+
+function avaliarAluno(nome, n1, n2, n3){
+    console.log('Nome: '+nome+' Media: '+calcularMedia(n1,n2,n3)+' Situação: '+verificarSituacao(calcularMedia(n1,n2,n3)))
+}
+
+function geraSenha(){
+   console.log(String.fromCharCode(Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33,Math.floor(Math.random() * 126) + 33))
+}
